6375002f75d2e1688d2d76c5cd7790dd03ae0d4e
LLath <rothpatrick12@yahoo.de>
2020-04-07 17:59:17 +0200

    Updates

---
+++ precache-manifest.7cf204545c0eec5ebc5111439bd70254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58913c15f5e517e17ff7605152ad6aff",
+    "url": "/DnD/index.html"
+  },
+  {
+    "revision": "da8defa139cdf35efea9",
+    "url": "/DnD/static/js/2.c95246b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DnD/static/js/2.c95246b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273f7063f82a0b31fedd",
+    "url": "/DnD/static/js/main.f747066b.chunk.js"
+  },
+  {
+    "revision": "1bcd5f67c25a1b45b172",
+    "url": "/DnD/static/js/runtime-main.12e15ace.js"
+  }
+]);
